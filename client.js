34f4a6152fb64736d4443353f39be37470d35839
zichenghuangFrank <zichenghuang05@gmail.com>
2020-03-03 17:40:38 -0700

    Add files via upload

--- client.js
+++ client.js
@@ -1,12 +1,19 @@
 $(function () {
     var socket = io();
+    let $chatH = $('#chat-history');
+    let $userOn = $('#online-users');
+    let $msgInput = $('#msg-send');
+
     $('form').submit(function(e){
         e.preventDefault(); // prevents page reloading
-        socket.emit('chat message', $('#msg-send').val());
-        $('#msg-send').val('');
+        socket.emit('chat message', {msg: $msgInput.val()});
+        $msgInput.val('');
         return false;
     });
     socket.on('chat message', function(data){
-        $('#chat-history').append($('<li>').text(data));
+        console.log(data);
+        let date = new Date(data.time);
+        let date_format = date.toTimeString().split(" ");
+        $chatH.prepend($('<li>').text(date_format[0] + ' ' + data.msg));
     });
 });
--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var express = require('express');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var totalUserNumber = 0;
+var usersOnline = [];
+var usersAll = [];
+var chatLog = [];
+
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
@@ -11,15 +16,52 @@ app.get('/', function(req, res) {
 
 
 io.on('connection', function(socket){
+
+    var newUser = {};
+
+    socket.on('') {
+
+    }
+
     // Server receives the new chat message and handle from clients
     socket.on('chat message', function (data) {
-        io.emit('chat message', data);
-        let timestamp = Date.now();
+        let timestamp = Date.now(); // Add current time stamps
+        
+        
+        
+        io.emit('chat message', {time: timestamp, msg: data.msg});
+        
     });
+
+
+
+
+
+
     socket.on('disconnect', function () {
        console.log('user disconnected');
     });
 });
+
+// Find the index of online user name
+function findOnlineUserByName(name) {
+    for (let i = 0; i < usersOnline.length; i++) {
+        if (name === usersOnline[i]) {
+            return i
+        }
+    }
+    return -1;
+}
+
+// Check whether the name is unique or not
+function isUniqueName(name) {
+    for (let i = 0; i < usersAll.length; i++) {
+        if (name === usersAll[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
