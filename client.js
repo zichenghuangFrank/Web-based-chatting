2a214d936b23832d748afa6c6f962af446b5350c
zichenghuangFrank <zichenghuang05@gmail.com>
2020-03-05 20:50:25 -0700

    second version

--- client.js
+++ client.js
@@ -1,37 +1,54 @@
  $(function () {
     var socket = io();
-    let onlineFlag = false;
+    // let onlineFlag = false;
     let $chatH = $('#chat-history');
     let $usersOnline = $('#online-users');
     let $msgInput = $('#msg-send');
+    var nickname = "";
 
-    let curName = $.cookie("userName");
-    let curColor = $.cookie("userColor");
+    var curName = $.cookie("userName");
+    var curColor = $.cookie("userColor");
 
     socket.on('connect', function(data) {
-        socket.emit('user connect', curName, curColor);
+        socket.emit('user connect', curName, curColor, function (data) {
+            $.cookie("userName", data.name);
+            $.cookie("userColor", data.color);
+            console.log(data);
+        });
     });
 
+    // Assign the user in the chatting room
     socket.on('send nickname', function(data){
+        $.cookie("userName", data.userName);
+        $.cookie("userColor", data.userColor);
+        nickname = data.userName;
         $('#user-name')[0].innerHTML = "You are " + "<a style='color: " + data.userColor + "'>" + data.userName + "</a>";
     });
 
+    // Update the online list
+    socket.on('update online list', function(data){
+        $usersOnline.empty();
+        for (let i = 0; i < data.length; i++) {
+            $usersOnline.prepend("<li style='color: " + data[i].userColor + "'>" + data[i].userName + "</li>");
+        }
+    });
 
-
+    // Click the 'send' button
     $('form').submit(function(e){
         e.preventDefault(); // prevents page reloading
-        socket.emit('chat message', {msg: $msgInput.val()});
+        socket.emit('chat message', {msg: $msgInput.val(), userName: nickname});
         $msgInput.val('');
         return false;
     });
 
-    socket.on('chat message', function(data){
+    socket.on('show chat message', function(data){
         console.log(data);
-        // if (onlineFlag === false) {
-        //     $('#user-name').val() 
-        // }
         let date = new Date(data.time);
         let date_format = date.toTimeString().split(" ");
-        $chatH.prepend($('<li>').text(date_format[0] + ' ' + data.msg));
+        if (!data.userName) {
+            $chatH.prepend($('<li>').text(date_format[0] + ' ' + data.msg));    
+        } else {
+            $chatH.prepend($('<li>').text(date_format[0] + ' ' + data.userName + ' ' + data.msg));
+        }
     });
 });
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ io.on('connection', function(socket){
 
     var newUser = {};
 
-    socket.on('user connect', function(userName, userColor) {
+    socket.on('user connect', function(userName, userColor, cookieData) {
         if(!userName) {
             let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16); 
             newUser.userColor = randomColor;
@@ -31,7 +31,9 @@ io.on('connection', function(socket){
             newUser.userName = userName; 
         }
         
-        newUser.id = socket.id;
+        newUser.id = socket.id; // Get the user socket id 
+        cookieData({name: newUser.userName, color: newUser.userColor}); // Update the cookie data
+
         usersOnline.push(newUser);
         usersAll.push(newUser);
 
@@ -43,30 +45,106 @@ io.on('connection', function(socket){
     socket.on('chat message', function (data) {
         let timestamp = Date.now(); // Add current time stamps
         let userIndex = findOnlineUserByName(data.userName);
-        let userName  = usersOnline[userIndex];
+        let userOnline  = usersOnline[userIndex];
+        console.log(userOnline);
 
         // Check if the command is /nick or /nickcolor 
-        let commandMessage = data.msg.split(' ');
-        
+        let commandMessage = data.msg.split(" ");
+        console.log(commandMessage);
 
-        io.emit('chat message', {time: timestamp, userName: userName, msg: data.msg});
+        // Change name command
+        if (commandMessage[0] === "/nick" && commandMessage.length > 1) {
+
+            let curName = commandMessage.shift();
+            // Empty name
+            if (curName.trim().length === 0) {
+                socket.emit('show chat message', {time: timestamp, msg: "You enter an invalid command! Please try again."});
+                return;
+            }
+
+            // Check whether the changed name is unique or not 
+            if (!isUniqueName(curName)) {
+                socket.emit('show chat message', {time: timestamp, msg: "Your nickname has been taken! Please try again."});
+                return;
+            }
+
+            // Change into the new name
+            let preName = userOnline.userName;
+            usersOnline[userIndex].userName = curName;
+            socket.emit('send nickname', {userName: curName, userColor: userOnline.userColor});
+
+            // Update the online list
+            io.emit("update online list", usersOnline);
+            
+            // Update all users' list
+            updateAllUserByName(preName, curName);
+
+            // Update the chat history
+            
+
+            // Send the message
+            socket.emit('show chat message', {time: timestamp, msg: "You have changed your nickname!"});
         
+        } else if (commandMessage[0] === "/nickcolor" && commandMessage.length === 2) { // Change color command
+            // Invalid command 
+            if (commandMessage[1].length !== 6 || parseInt("0x" + commandMessage[1]) > 0xFFFFFF) {
+                socket.emit('show chat message', {time: timestamp, msg: "Your change color should be like RRGGBB! Please try again."});
+                return;
+            }
+            
+            // Change into the new nickname color
+            let curColor = "#" + commandMessage[1];
+            usersOnline[userIndex].userColor = curColor;
+            socket.emit('send nickname', {userName: curName, userColor: userOnline.userColor});
+
+            // Update the online list
+            io.emit("update online list", usersOnline);
+
+            // Update all users' list
+            updateAllUserByColor(curColor, userOnline);
+
+            // Update the chat history
+
+             // Send the message
+             socket.emit('show chat message', {time: timestamp, msg: "You have changed your nickname color!"});
+        } else {
+            
+            // Send the regular message
+            io.emit('show chat message', {time: timestamp, userName: userOnline.userName, msg: data.msg});
+        }
+
     });
-
-
-
-
-
 
     socket.on('disconnect', function () {
        console.log('user disconnected');
     });
+
 });
+
+// Update the user's name in all users
+function updateAllUserByName(preName, curName) {
+    for (let i = 0; i < usersAll.length; i++) {
+        if (preName === usersAll[i].userName) {
+            usersAll[i].userName = curName;
+            return;
+        }
+    }
+}
+
+// Update the user's nickname color in all users
+function updateAllUserByColor(curColor, name) {
+    for (let i = 0; i < usersAll.length; i++) {
+        if (usersAll[i].userName === name) {
+            usersAll[i].userColor = curColor;
+            return;
+        }
+    }
+}
 
 // Find the index of online user name
 function findOnlineUserByName(name) {
     for (let i = 0; i < usersOnline.length; i++) {
-        if (name === usersOnline[i]) {
+        if (name === usersOnline[i].userName) {
             return i
         }
     }
@@ -76,12 +154,23 @@ function findOnlineUserByName(name) {
 // Check whether the name is unique or not
 function isUniqueName(name) {
     for (let i = 0; i < usersAll.length; i++) {
-        if (name === usersAll[i]) {
+        if (name === usersAll[i].userName) {
             return false;
         }
     }
     return true;
 }
+
+// Find the socket id of the index of each user
+function findOnlineUserById(socketId) {
+    for (let i = 0; i < usersOnline.length; i++) {
+        if (socketId === usersOnline[i].id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
